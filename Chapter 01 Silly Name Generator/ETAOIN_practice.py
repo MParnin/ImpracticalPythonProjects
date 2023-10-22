ca16e2ab7fc8959595f4a3a4f35634a6819a08f2
M Parnin <MParnin@protonmail.com>
2023-10-22 18:41:30 -0400

    Initial commit

---
+++ Chapter 01 Silly Name Generator/ETAOIN_practice.py
@@ -0,0 +1,21 @@
+"""Map letters from string into dictionary & print bar chart of frequency"""
+import sys
+import pprint
+from collections import defaultdict
+
+# Note: text should be a short phrase for bars to fit in IDLE window
+text = 'Like the castle in its corner in a medieval game, I foresee terrible \
+    trouble and I stay here just the same.'
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+mapped = defaultdict(list)
+for character in text:
+    character = character.lower()
+    if character in ALPHABET:
+        mapped[character].append(character)
+
+print("\nYou may need to strech console window if text wrapping occurs.\n")
+print("text = ", end='')
+print("{}\n".format(text), file=sys.stderr)
+pprint.pprint(mapped, width=110)
